b43f9efeefbe6a97418ea5d9d5a9fc1272670913
Tyler Abele <tabele17@gmail.com>
2024-10-27 13:21:23 -0400

    changed the build.yaml file a bit

--- app/main.py
+++ app/main.py
@@ -10,8 +10,16 @@ app = FastAPI()
 
 @app.get("/")  # zone apex
 def zone_apex():
-    return {"Hello": "Hello API"}
+    return {"hello world:>>>"}
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
+
+@app.get("/multiply/{c}/{d}")
+def multiply(c:int, d: int):
+    return {"product": c*d}
+
+@app.get("/square/{a}")
+def square(a: float):  # sourcery skip: square-identity
+    return {"square": a*a}
