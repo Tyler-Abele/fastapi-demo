4aa85045c0ec2eb72d0ccf3de730af492af19094
Tyler Abele <tabele17@gmail.com>
2024-12-03 14:35:19 -0500

    fixed some errors

--- app/main.py
+++ app/main.py
@@ -26,49 +26,49 @@ DBPASS = os.getenv('DBPASS')
 DB = "xxe9ff"
 
 #comment so that I can re-run
-@app.get("/genres")
-async def get_genres():
-    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-    cur=db.cursor()
-    query = "SELECT * FROM genres ORDER BY genreid;"
-    try:
-        cur.execute(query)
-        headers=[x[0] for x in cur.description]
-        results = cur.fetchall()
-        json_data=[] 
-        for result in results:
-            json_data.append(dict(zip(headers,result)))
-        cur.close()
-        db.close()
-        return(json_data)   
-    except Error as e:
-        cur.close()
-        db.close()
-        return {"Error": "MySQL error: " + str(e)}
-
-@app.get('/songs')
-async def get_songs():
-    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-    cur=db.cursor()
-    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid;"
-    try:
-        cur.execute(query)
-        
-        results = cur.fetchall()
-        headers=[x[0] for x in cur.description]
-        json_data = []
-        for result in results:
-            json_data.append(dict(zip(headers,result)))
-        cur.close()
-        db.close()
-        return{"songs" :json_data}
-    except Error as e:
-        cur.close()
-        db.close()
-        return{"Error" :"MySQL error: " + str(e)}
-
 
 @app.get("/")  # zone apex
 def zone_apex():
     return {"howdy"}
 
+@app.get('/genres')
+async def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
+    cur = db.cursor()
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        cur.close()
+        db.close()
+        return {"Error": "MySQL Error: " + str(e)}
+
+@app.get('/songs')
+async def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
+    cur = db.cursor()
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid;"
+    try:
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        cur.close()
+        db.close()
+        return None
